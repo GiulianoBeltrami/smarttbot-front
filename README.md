1b92deef0fd309bcb4ae677f669a4970d7d8eb29
Giuliano Beltrami <juka2027@live.com>
2022-02-20 19:43:30 -0300

    Fix tests and readme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Desenvolvimento : React, redux e bootstrap<br />
 Testes: testing-library(jest)<br />
 Deploy: netlify<br />
+Api: https://api-front-test.k8s.smarttbot.com/api-docs/ <br />
 # Como executar
 Baixar o código fonte no github, navergar até a pasta do aplicativo e digitar no cmd: npm start
 
@@ -14,3 +15,9 @@ src<br />
   --> /assets/ -> pasta que contém imagens<br />
   --> /services/ -> possui toda a lógica responsável por chamar a api utilizando redux<br />
   --> /tests/ -> pasta que possui todos os testes do programa<br />
+
+# Endpoints utilizados
+
+/api/v1/robot/overview <br />
+/api/v1/strategy <br />
+/api/v1/robot <br />
--- src/tests/ModalRobot/ModalRobot.test.js
+++ src/tests/ModalRobot/ModalRobot.test.js
@@ -14,35 +14,36 @@ const testData = {
 const handleClose = jest.fn();
 const onSubmit = jest.fn(e => e.preventDefault());
 
-test('show modal correctly', async () => {
+const input = { "title": "", "initial_capital": "", "strategy_id": "", "mode": 0, "simulation": 0, "broker_id": 1 };
+const setInput = jest.fn();
 
-    const { container } = render(<ModalRobot show={true} handleClose={handleClose} onSubmit={onSubmit} data={testData} />);
+test('show modal correctly', async () => {
+    const { container } = render(<ModalRobot show={true} handleClose={handleClose} onSubmit={onSubmit} data={testData} inputs={input} setInputs={setInput}/>);
     expect(container).toMatchSnapshot();
     expect(await screen.findByText('Vamos criar seu robô')).toBeInTheDocument();
 
 });
 
 test('close modal correctly', async () => {
-    const { container } = render(<ModalRobot show={true} handleClose={handleClose} onSubmit={onSubmit} data={testData} />);
+    const { container } = render(<ModalRobot show={true} handleClose={handleClose} onSubmit={onSubmit} data={testData} inputs={input} setInputs={setInput}/>);
     expect(container).toMatchSnapshot();
     fireEvent.click(await screen.findByTestId('closeModal'));
     expect(handleClose).toHaveBeenCalledTimes(1);
 });
 
 test('render Adicionar novo Robô text', async () => {
-    const { container } = render(<ModalRobot show={true} handleClose={handleClose} onSubmit={onSubmit} data={testData} />);
+    const { container } = render(<ModalRobot show={true} handleClose={handleClose} onSubmit={onSubmit} data={testData} inputs={input} setInputs={setInput}/>);
     expect(container).toMatchSnapshot();
     const textElement = await screen.findByText('Adicionar novo Robô');
     expect(textElement).toBeInTheDocument();
 })
 
 
-test('form with valid inputs', async () => {
-    const handleSubmit = jest.fn(e => e.preventDefault());
-    const { container } = await render(<ModalRobot show={true} handleClose={handleClose} onSubmit={handleSubmit} data={testData} />);
+test('form with valid inputs', () => {
+    const { container } = render(<ModalRobot show={true} handleClose={handleClose} onSubmit={onSubmit} data={testData} inputs={input} setInputs={setInput}/>);
     expect(container).toMatchSnapshot();
     fireEvent.change(screen.getByLabelText('Nome do produto'), { target: { value: "Robo1" } });
     fireEvent.change(screen.getByLabelText('Capital inicial do robô'), { target: { value: "250" } });
     fireEvent.click(screen.getByText('Criar robô'))
-    expect(handleSubmit).toHaveBeenCalledTimes(1);
+    expect(onSubmit).toHaveBeenCalledTimes(1);
 })
